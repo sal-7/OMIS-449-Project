0c9760aa47cc42f398b14eba21a054e4f8d6931e
Sal <harbifa@gmail.com>
2019-04-23 17:17:39 -0500

    449 Generator
    files for project

---
+++ project/PracticeGenerator.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace project
+{
+    partial class PracticeGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(415, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(589, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Practice Generator";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(119, 262);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(316, 579);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.generateBtn.Location = new System.Drawing.Point(599, 305);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(177, 73);
+            this.generateBtn.TabIndex = 2;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.clearBtn.Location = new System.Drawing.Point(599, 739);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(177, 73);
+            this.clearBtn.TabIndex = 3;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(903, 262);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(486, 579);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1071, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Practice Schedule:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Teams:";
+            // 
+            // PracticeGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1494, 1050);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "PracticeGenerator";
+            this.Text = "Practice Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ project/PracticeGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class PracticeGenerator : Form
+    {
+        public PracticeGenerator()
+        {
+            InitializeComponent();
+            loadData();
+       
+            listBox2.Items.Add("");
+            listBox2.Items.Add("");
+        }
+
+        private void loadData()
+        {
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            var file = new System.IO.StreamReader(@"file.txt");
+            String line;
+            while ((line = file.ReadLine()) != null)
+            {
+                listBox1.Items.Add(line);
+            }
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox2.Items.Add("");
+            listBox2.Items.Add("");
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+            DateTime time = new DateTime(1, 1, 1, 8, 0, 0); //initialize with 8 am
+          
+
+            foreach (var item in listBox1.SelectedItems)
+            {
+                var interval1 = time.ToString("hh:mm tt");   // this show  like am/pm
+                DateTime updatedTime = time.AddMinutes(30);
+                var interval2 = updatedTime.ToString("hh:mm tt");   // this show  am/pm
+                listBox2.Items.Add(interval1 + " - "+ interval2+"    " + item.ToString());
+                time = updatedTime;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
